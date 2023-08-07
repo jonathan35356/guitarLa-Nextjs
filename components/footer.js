8e70a3cd62bcbe26298bc493901b896fafd02c19
jonathan35356 <j.javier200211@outlook.com>
2023-08-07 12:50:11 -0500

    se agrego el footer y la pagina de nosotros

--- components/footer.js
+++ components/footer.js
@@ -3,13 +3,18 @@ import styles from "../styles/footer.module.css";
 
 export default function Footer() {
   return (
-    <div className="contenedor">
-      <nav className="navegacion">
-        <Link href="/">Inicio</Link>
-        <Link href="/nosotros">Nosotros</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/tienda">Tienda</Link>
-      </nav>
-    </div>
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
+        <p className={styles.copyright}>
+          Todos los derechos reservados {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
   );
 }
