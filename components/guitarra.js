cac2250c70374a5c9db89e2a5766b56cf1fa4070
jonathan35356 <j.javier200211@outlook.com>
2023-08-11 20:35:47 -0500

    ultimo commit

--- components/guitarra.js
+++ components/guitarra.js
@@ -15,7 +15,7 @@ export default function Guitarra({ guitarra }) {
       <div className={styles.contenido}>
         <h3>{nombre}</h3>
         <p className={styles.descripcion}>{descripcion}</p>
-        <p className={styles.precio}>{precio}</p>
+        <p className={styles.precio}>$ {precio}</p>
         <Link href={`/guitarras/${url}`} className={styles.enlace}>
           Ver Producto
         </Link>
